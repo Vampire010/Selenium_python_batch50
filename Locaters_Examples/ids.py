1221d4dd9b8843687184eff77a00ff58c990afcd
girishrathode.rathode@gmail.com <girishrathode.rathode@gmail.com>
2021-04-17 09:12:40 +0530

    17 - 04 - 2021

--- Locaters_Examples/ids.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Chrome("/Users/girishg/PycharmProjects/Kids_Bank/Browser_Drivers/chromedriver")
-driver.get("https://parabank.parasoft.com/parabank/register.htm")
-driver.maximize_window()
-driver.find_element(By.ID, "customer.firstName").send_keys(1221313)
-driver.find_element(By.ID, "customer.lastName").send_keys("Daniel")
-
